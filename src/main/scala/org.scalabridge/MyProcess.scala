f9a682d6c80c96cdfd66c8cc3be9756da81323cd
Michelle Austin <michelletoelle@gmail.com>
2019-07-27 22:04:02 +0100

    refactor

--- src/main/scala/org.scalabridge/MyProcess.scala
+++ src/main/scala/org.scalabridge/MyProcess.scala
@@ -1,34 +1,9 @@
 package org.scalabridge
 
-import java.time.DayOfWeek
-import java.util.Calendar
-import java.util.UUID.randomUUID
-import java.util.concurrent.TimeUnit
-
-import com.danielasfregola.twitter4s.TwitterRestClient
-import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
-import com.typesafe.scalalogging.Logger
 import doodle.core.{Angle, Color}
-import doodle.effect.Writer.Png
 import doodle.image.Image
 import doodle.random.Random
 
-import scala.concurrent.ExecutionContext
-import Utils.addThenDoubleIt
-import doodle.image._
-import doodle.image.syntax._
-import doodle.core._
-import doodle.java2d._
-import doodle.effect.Writer._
-import doodle.java2d.effect.Frame
-import doodle.random.Random
-import org.apache.commons.codec.binary.Base64
-import org.json4s._
-import org.json4s.JsonDSL._
-
-import scala.io.{BufferedSource, Source}
-import java.nio.file.{Files, Paths}
-import java.time.DayOfWeek
 import java.util.{Calendar, TimeZone, Timer, TimerTask}
 import java.util.UUID.randomUUID
 import java.util.concurrent.TimeUnit
@@ -36,74 +11,91 @@ import java.util.concurrent.TimeUnit
 import com.danielasfregola.twitter4s.TwitterRestClient
 import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
 
-import scala.concurrent.ExecutionContext
-import com.typesafe.scalalogging._
 
-class MyProcess {
+sealed trait Tweet {
+  val consumerToken = ConsumerToken(key = "wVEpDxPf1XRmvYmrZHgvULaGM", secret = "7q9Z1DuogwyZtdeYkkgxzkmIW8qTcfJRH0x9IXUFNw5gmJtNEs")
+  val accessToken = AccessToken(key = "3317308574-mgg8AWsjQoLhReURzpIZ3htV9t3dMfsNNEBcvBF", secret = "GOyWnhDoq8imihe47y2E6BsTrLvOxQjz3P2HKlAZkHdrA")
+  val restClient = TwitterRestClient(consumerToken, accessToken)
 
-    case object Twitter {
-      //set tokens for twitter4s
-      val consumerToken = ConsumerToken(key = "wVEpDxPf1XRmvYmrZHgvULaGM", secret = "7q9Z1DuogwyZtdeYkkgxzkmIW8qTcfJRH0x9IXUFNw5gmJtNEs")
-      val accessToken = AccessToken(key = "3317308574-mgg8AWsjQoLhReURzpIZ3htV9t3dMfsNNEBcvBF", secret = "GOyWnhDoq8imihe47y2E6BsTrLvOxQjz3P2HKlAZkHdrA")
-      val restClient = TwitterRestClient(consumerToken, accessToken)
-    }
+  //def image(count: Int, color: Color, size: Double, today: String): Random[Image] ={
+    //TODO fix this - how to create the image here
+    //ImageBuilder.dayImage(count, color, size, today).run.write[Png](filePath)
+  //}
 
-    def getNumOfDay (now: Calendar): Int = {
-       now.get(Calendar.DAY_OF_WEEK)
-    }
+  def filePath(): String = {
+    "/tmp/" + randomUUID().toString + ".png"
+  }
+  def status(today: String): String = {
+    "Happy " + today + "!"
+  }
+}
 
-    def nextColor(color: Color): Random[Color] = {
-      //building blocks for the imageS, selects the next color to be used
-      val spin = Random.normal(15.0, 10.0)
-      spin map { s => color.spin(Angle(s)) }
-    }
+sealed trait ImageFeatures {
+  def color: Color
+}
 
-    def coloredCircle(color: Color, size: Double): Image = {
-      //building blocks for images, generates a colored circle
-      Image.circle(size).strokeWidth(1.0).strokeColor(color.spin(Angle(180))).fillColor(color)
-    }
+final case class NextColor(color: Color, spin: Random[Double] = Random.normal(15.0, 10.0) ) extends ImageFeatures {
+  def apply = spin map { s => color.spin(Angle(s)) }
+}
+final case class ColoredCircle(color: Color, size: Double) extends ImageFeatures {
+  def apply = Image.circle(size).strokeWidth(1.0).strokeColor(color.spin(Angle(180))).fillColor(color)
+}
 
-    def today (numOfDay: Int): String = {
-      numOfDay match {
-        case 1 => "Sunday"
-        case 2 => "Monday"
-        case 3 => "Tuesday"
-        case 4 => "Wednesday"
-        case 5 => "Thursday"
-        case 6 => "Friday"
-        case 7 => "Saturday"
-      }
-    }
 
-  def dayImage(count: Int, color: Color, size: Double, dayOfWeek: Int): Random[Image] = {
+
+//TODO need to refactor this section to get rid of the duplicate methods and to refactor the dayimage method
+class ImageBuilder(count: Int, color: Color, size: Double, today: String) {
+
+
+  def nextColor(color: Color): Random[Color] = {
+    //building blocks for the imageS, selects the next color to be used
+    val spin = Random.normal(15.0, 10.0)
+    spin map { s => color.spin(Angle(s)) }
+  }
+
+  def coloredCircle(color: Color, size: Double): Image = {
+    //building blocks for images, generates a colored circle
+    Image.circle(size).strokeWidth(1.0).strokeColor(color.spin(Angle(180))).fillColor(color)
+  }
+
+  def dayImage(count: Int, color: Color, size: Double, today:String): Random[Image] = {
     //create an image based on day of week
     count match {
       case 0 => Random.always(Image.empty)
       case n =>
         val loop = coloredCircle(color, size)
-        val loops = nextColor(color) flatMap { c => dayImage(n - 1, c, size + scala.util.Random.nextInt(7), dayOfWeek) }
-        dayOfWeek match {
-          case 1 => loops map { l => (loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop) on l }
-          case 2 => loops map { l => loop above (loop beside loop beside loop) above loop on l }
-          case 3 => loops map { l => (loop beside loop) below loop on l }
-          case 4 => loops map { l => (loop beside loop beside loop beside loop beside loop) on l }
-          case 5 => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
-          case 6 => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
-          case 7 => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
+        val loops = nextColor(color) flatMap { c => dayImage(n - 1, c, size + scala.util.Random.nextInt(7), today) }
+        today match {
+          case "Sunday" => loops map { l => (loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop) on l }
+          case "Monday" => loops map { l => loop above (loop beside loop beside loop) above loop on l }
+          case "Tuesday" => loops map { l => (loop beside loop) below loop on l }
+          case "Wednesday" => loops map { l => (loop beside loop beside loop beside loop beside loop) on l }
+          case "Thursday" => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
+          case "Friday" => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
+          case "Saturday" => loops map { l => loop above (loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop beside loop beside loop) above (loop beside loop beside loop) above (loop beside loop beside loop) above loop on l }
         }
     }
   }
+}
 
-
-  def filePath(): String = {
-    "/tmp/" + randomUUID().toString + ".png"
+class today {
+  def apply(now:Int): String =  {
+    this match {
+      case 1 => "Sunday"
+      case 2 => "Monday"
+      case 3 => "Tuesday"
+      case 4 => "Wednesday"
+      case 5 => "Thursday"
+      case 6 => "Friday"
+      case 7 => "Saturday"
+    }
   }
+}
 
-  def status(today: String): String = {
-  "Happy " + today + "!"
-  }
+//TODO move timer task and schedule task  sections to the main method
 
-/*
+ /*
+ object TimerTask {
     val task: TimerTask = new java.util.TimerTask {
       //queue up the tweet
       def run() = {
@@ -114,14 +106,17 @@ class MyProcess {
         } yield tweet
       }
     }
+    }
 */
-    def scheduleTweet(task: TimerTask, now: Calendar): Unit = {
-    //schedule the tweet and repeat
-    val t = new java.util.Timer()
-    t.schedule(task, now.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS))
-  }
+  object scheduleTweet {
+   def apply(task: TimerTask, now: Calendar): Unit = {
+     //schedule the tweet and repeat
+     val t = new java.util.Timer()
+     t.schedule(task, now.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS))
+   }
+ }
 
-  }
+
 
 
 
--- src/test/scala/Maintest.scala
+++ src/test/scala/Maintest.scala
@@ -1,10 +1,11 @@
-import org.scalabridge.MyProcess
+import org.scalabridge.today
 import org.scalatest.FlatSpec
 
+/*
 
 class SetSpec extends FlatSpec {
   "dayOfWeek method" should "get a calendar instance and determine the day of the week" in {
-    val todayTest = new MyProcess
+    val todayTest = new today.apply
     assert(todayTest.today(1) == "Sunday")
     assert(todayTest.today(2) == "Monday")
     assert(todayTest.today(3) == "Tuesday")
@@ -15,5 +16,6 @@ class SetSpec extends FlatSpec {
     }
   }
 
+*/
 
 
